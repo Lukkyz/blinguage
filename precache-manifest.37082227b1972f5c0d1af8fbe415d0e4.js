a636340ed68e46fb286597663f97a7e04329bb72
Lukkyz <bienaimelucas@hotmail.fr>
2020-10-01 14:02:24 +0200

    Updates

---
+++ precache-manifest.37082227b1972f5c0d1af8fbe415d0e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f4b4634ace27921fcd106e568bd409d",
+    "url": "/blinguage/index.html"
+  },
+  {
+    "revision": "57253ed1a48878e01d68",
+    "url": "/blinguage/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "58775a54452baf230028",
+    "url": "/blinguage/static/js/2.cdff10f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/blinguage/static/js/2.cdff10f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57253ed1a48878e01d68",
+    "url": "/blinguage/static/js/main.28c6e1f1.chunk.js"
+  },
+  {
+    "revision": "0f5157034a5a4f182713",
+    "url": "/blinguage/static/js/runtime-main.c8a68c4b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/blinguage/static/media/logo.5d5d9eef.svg"
+  }
+]);
